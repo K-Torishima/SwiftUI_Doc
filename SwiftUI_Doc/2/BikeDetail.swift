26a9e4d5ea84fd7b7b09c5e37926fe1327f80a6f
koji.torishima <torikou3685@gmail.com>
2020-03-15 16:28:23 +0900

    Commit

--- SwiftUI_Doc/2/BikeDetail.swift
+++ SwiftUI_Doc/2/BikeDetail.swift
@@ -12,16 +12,18 @@ struct BikeDetail: View {
     var bike: Bike
     var body: some View {
         VStack {
-            Section(header: Text("画像")) {
-                Image(bike.imageName)
-                    .resizable()
-                    .frame(height: 500)
-                    .scaledToFit()
-            }
-
+            
+            
             ZStack(alignment: .bottomTrailing) {
                 
                 Form {
+                    Section(header: Text("画像")) {
+                        Image(bike.imageName)
+                            .resizable()
+                            .frame(height: 500)
+                            .scaledToFit()
+                    }
+                    
                     Section(header: Text("タイプ")) {
                         Text(bike.type)
                     }
